586181537afc5d451f3ca896783935f3edfebfb1
George Doggo <Adminl@PARME22>
2018-08-07 17:11:00 +0200

    delegue

--- Delegue/Program.cs
+++ Delegue/Program.cs
@@ -8,8 +8,31 @@ namespace Delegue
 {
     class Program
     {
+        public delegate void monDelegate(string message);
         static void Main(string[] args)
         {
+            var instDelegue = new monDelegate(AfficherMessage);
+            instDelegue += AfficherMessageError;
+            TesteDelegue(" ", " ", instDelegue);
+        }
+        public static void TesteDelegue(
+            string a,
+            string b,
+            monDelegate methodePourAfficher)
+        {
+            methodePourAfficher("VOILA!");
+            
+        }
+
+        public static void AfficherMessage(string message)
+        {
+            Console.WriteLine($"Voila {message}");
+        }
+        public static void AfficherMessageError(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"Voila {message}");
+            Console.ForegroundColor = ConsoleColor.Black;
         }
     }
 }
